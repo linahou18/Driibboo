8f7ac74e275b921adf6e160ea130ba34ee041f38
linahou18 <linahou18@gmail.com>
2016-09-12 20:36:34 -0700

    Navigation Drawer

---
+++ Driibboo/app/src/main/java/com/example/nina/driibboo/model/Bucket.java
@@ -0,0 +1,16 @@
+package com.example.nina.driibboo.model;
+
+import java.util.Date;
+
+/**
+ * Created by nina on 9/11/16.
+ */
+public class Bucket {
+    public String id;
+    public String name;
+    public String description;
+    public int shots_count;
+    public Date created_at;
+
+    public boolean isChoosing;
+}
---
+++ Driibboo/app/src/main/java/com/example/nina/driibboo/model/User.java
@@ -0,0 +1,9 @@
+package com.example.nina.driibboo.model;
+
+/**
+ * Created by nina on 9/11/16.
+ */
+public class User {
+    public String name;
+    public String avatar_url;
+}
